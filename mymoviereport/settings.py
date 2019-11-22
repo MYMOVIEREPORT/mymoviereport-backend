851bc3e256d41208b9814eff277fc852c3bf62d7
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-11-22 09:30:31 +0900

    add API swagger

--- mymoviereport/settings.py
+++ mymoviereport/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'movies',
     'rest_framework',
     'corsheaders',
+    'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
