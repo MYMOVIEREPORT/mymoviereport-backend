fb62629939f1b8b07e64ddb0f2611a1251c2fee3
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-11-23 19:39:03 +0900

    modify syntax auto

--- accounts/models.py
+++ accounts/models.py
@@ -13,9 +13,8 @@ class User(AbstractUser):
                     MaxValueValidator(200, '올바른 나이를 입력해주세요.')
                     ),
         null=True
-        )  # 나이를 validator로 검증, 선택 정보
+    )  # 나이를 validator로 검증, 선택 정보
     thumbnail = models.CharField(max_length=500)  # 썸네일 기본값 필요
-    # genre_prefer = models.ManyToManyField(Genre, related_name='prefer_users')
 
     def __str__(self):
         return self.username
