147266432920888ddbed5827f2709ab253a65d14
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-11-26 10:18:41 +0900

    reupdate models

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,8 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class User(AbstractUser):
     age = models.IntegerField(
         validators=(MinValueValidator(0, '올바른 나이를 입력해주세요.'),
-                    MaxValueValidator(200, '올바른 나이를 입력해주세요.'))
+                    MaxValueValidator(200, '올바른 나이를 입력해주세요.')),
+        default=25
     )
     thumbnail = models.CharField(max_length=500)
 
