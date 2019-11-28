ca1b45df3e016a1043c8569c2b4dccb842e00595
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-11-28 16:10:42 +0900

    load static

--- mymoviereport/settings.py
+++ mymoviereport/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import django_heroku
 import os
 import datetime
 
@@ -148,3 +149,7 @@ JWT_AUTH = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
