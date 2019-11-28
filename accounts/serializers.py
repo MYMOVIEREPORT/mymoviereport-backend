888ebbf6d674caaa8ec532d97409eb34d2954e7f
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-11-28 18:27:48 +0900

    test login

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ['id', 'username', 'email', 'age', 'thumbnail', ]
+        fields = ['id', 'username', 'password', 'email', 'age', 'thumbnail', ]
 
 
 class UserChangeSerializer(serializers.ModelSerializer):
