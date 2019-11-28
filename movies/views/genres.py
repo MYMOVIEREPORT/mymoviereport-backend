bed005af43872f3ec93f67a1fb4b339fa3f675c9
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-11-28 10:23:07 +0900

    complete basic logic filtering movies

--- movies/views/genres.py
+++ movies/views/genres.py
@@ -4,18 +4,21 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
-from movies.serializers import GenreSerializer
+from movies.serializers import MovieSerializer
 from movies.models import Genre
 
 
 @api_view(['GET'])
 @permission_classes([AllowAny, ])
 def genres_entire(request):
+    page = int(request.GET.get('page'))
     target = request.GET.get('target')
     if target:
         genre = get_object_or_404(Genre, id=target)
-        serializer = GenreSerializer(genre)
+        movies = genre.movies.all()[24 * (page - 1): 24 * page]
+        serializer = MovieSerializer(movies, many=True)
     else:
         genres = Genre.objects.all()
-        serializer = GenreSerializer(genres, many=True)
+        movies = genre.movies.all()[24 * (page - 1): 24 * page]
+        serializer = MovieSerializer(movies, many=True)
     return JsonResponse(serializer.data, safe=False)
